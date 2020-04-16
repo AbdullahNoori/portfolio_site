9c8eed81a2aee4277b142c8501e24ae5b9bcdf64
AbdullahNoori <nooriabdullah86@gmail.com>
2020-04-15 20:24:18 -0700

    readme'

--- portfolio/views.py
+++ portfolio/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(request): 
     # print ('portfolio')
-    return HttpResponse("portfolio home")
+    return HttpResponse("portfolio home page")
 
 def contact(request):
     return HttpRequest("Contact me")
